916d3216a181aebb67716b3518e2fc30a0cead72
Ivan(PC de clase) <email>
2024-11-12 13:54:35 +0100

    dummy prefabs y marcadores de jugadores en el minimapa

--- Assets/Scripts/UI/Game/Crosshair.cs
+++ Assets/Scripts/UI/Game/Crosshair.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Crosshair", menuName = "ScriptableObjects/Crosshair", order = 1)]
+[CreateAssetMenu(fileName = "Crosshair", menuName = "ScriptableObject/Crosshair", order = 1)]
 public class Crosshair : ScriptableObject
 {
     [SerializeField] private Sprite _sprite;
