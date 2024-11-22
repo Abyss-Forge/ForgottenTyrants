7872b93c91adbd0e6cb289092a5a8eb2332452d6
diegogb1999 <diegogb1999@gmail.com>
2024-11-22 02:02:23 +0100

    Network

---
+++ Assets/DiegoGB/NetworkManagerUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] Button _serverButton;
+    [SerializeField] Button _hostButton;
+    [SerializeField] Button _clientButton;
+
+    private void Awake()
+    {
+        _serverButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        _hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        _clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
